5a2447b5ad45bfac07b1b22fc6884bc819bf5d68
The Freezing Chicken <thefreezingchicken+github@gmail.com>
2023-12-13 23:00:37 +0100

    Sync

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,17 +1,16 @@
-import {addAllImgLoadEventListener, DIAGRAM_SIZE, DiagramGroup, LegendGroup} from './relay-diagram.js';
-import * as OP from "./op-lib.js";
+// import {addAllImgLoadEventListener, DIAGRAM_SIZE, DiagramGroup, LegendGroup} from './relay-diagram.js';
 import {OpType} from "./op-lib.js";
 
 
 const DIAGRAM_SETTINGS_STORAGE_KEY = 'diagram_settings';
 const OP_TYPE_INPUTS_STORAGE_KEY = 'op_type_inputs';
 
-const CHANGE_EVENT = new Event('change');
 
 /**
  * @class
  */
 class StoredInputs extends EventTarget {
+    _storedInputsObject;
     
     /**
      *
@@ -22,10 +21,12 @@ class StoredInputs extends EventTarget {
     constructor(storage, jsonObjectKey, ...elements) {
         super();
         
-        const storedInputsObject = JSON.parse(storage.getItem(jsonObjectKey));
-        this.#storedInputsObject = storedInputsObject;
+        const storedInputsObject = JSON.parse(storage.getItem(jsonObjectKey)) ?? {};
+        this._storedInputsObject = storedInputsObject;
     
         for (const e of elements) {
+            console.log(`Setting up element '${e.id}...'`);
+            
             const storedValue = storedInputsObject[e.id];
             
             if (storedValue != null) {
@@ -39,6 +40,8 @@ class StoredInputs extends EventTarget {
             console.log(`Stored value for ${e.id} is ${storedValue}`);
             
             e.addEventListener('change', () => {
+                console.log(`Element ${e.id} changed.`);
+                
                 let value;
                 if (e instanceof HTMLInputElement && e.type === 'checkbox') {
                     value = e.checked
@@ -48,7 +51,7 @@ class StoredInputs extends EventTarget {
                 storedInputsObject[e.id] = value;
                 
                 console.log(
-                    `Storing value ${value} for element ${e.id}`,
+                    `Storing value ${value} for element ${e.id}\n`,
                     `Stored value: ${storedInputsObject[e.id]}`
                 );
                 
@@ -57,7 +60,7 @@ class StoredInputs extends EventTarget {
                     JSON.stringify(storedInputsObject)
                 );
                 
-                this.dispatchEvent(CHANGE_EVENT);
+                this.dispatchEvent(new Event('change'));
             });
         }
     }
@@ -70,6 +73,8 @@ class StoredInputs extends EventTarget {
 class DiagramSettings extends StoredInputs {
     
     constructor() {
+        console.log("Building diagram settings...");
+        
         const functionsStyleSelect = document.getElementById('functions-style');
         const hideAnimalOrderCheckbox = document.getElementById('hide-numeric-animal-order');
         
@@ -83,9 +88,27 @@ class DiagramSettings extends StoredInputs {
  * @class
  */
 class OpTypeInputs extends StoredInputs {
+    /** @type {HTMLSelectElement} */
+    #animalStackSelect;
+    /** @type {HTMLSelectElement} */
+    #saviorsSelect;
+    /** @type {HTMLSelectElement} */
+    #modalitySelect;
+    
     #lastFirstAnimal;
     
+    /**
+     * @param optGroup {HTMLOptGroupElement}
+     * @param textAndValue {string}
+     */
+    appendSaviorsOption(optGroup, textAndValue) {
+        const isSelected = this._storedInputsObject[this.#saviorsSelect.id] === textAndValue;
+        optGroup.appendChild(new Option(textAndValue, textAndValue, null, isSelected));
+    }
+    
     constructor() {
+        console.log("Building OP type inputs...");
+        
         /** @type {HTMLSelectElement} */
         const animalStackSelect = document.getElementById('animals');
         /** @type {HTMLSelectElement} */
@@ -98,95 +121,111 @@ class OpTypeInputs extends StoredInputs {
         const noAnimalsValue = animalStackSelect.options[0].value;
         const noSaviorsValue = saviorsSelect.options[0].value;
     
+        
+        super(sessionStorage, OP_TYPE_INPUTS_STORAGE_KEY, animalStackSelect, saviorsSelect, modalitySelect);
+    
+        
+        this.#animalStackSelect = animalStackSelect;
+        this.#saviorsSelect = saviorsSelect;
+        this.#modalitySelect = modalitySelect;
+        // Setting to first letter of "NONE" value to make the listener below work correctly.
+        this.#lastFirstAnimal = noAnimalsValue[0];
+    
+    
         // Listen for changes of animal stack selection and fill savior selection options accordingly.
         animalStackSelect.addEventListener('change', () => {
+            console.log("Checking if saviors list needs refilling...");
+        
             // Get the selected value from 'animals' select
             const selectedStack = animalStackSelect.value;
-            
+        
             const isAnimalStackSelected = selectedStack !== noAnimalsValue;
             const newFirstAnimal = selectedStack[0];
-            
+        
             // If new selection matches last selection don't do anything and return.
-            if (this.#lastFirstAnimal === newFirstAnimal) return;
+            if (this.#lastFirstAnimal === newFirstAnimal) {
+                console.log("Nope.");
+                return;
+            }
             this.#lastFirstAnimal = newFirstAnimal;
             
+            console.log("Yep, it does. Refilling list...");
+        
             // Determine appropriate "NONE" value text.
             const noSaviorsText = isAnimalStackSelected ? '-- CHOOSE --' : '-- ☝🏻 --';
-            
+        
             // Reset <select> options.
             saviorsSelect.innerHTML = '';
         
             // Add "NONE" value.
-            saviorsSelect.add(new Option(noSaviorsValue, noSaviorsText));
-            
+            saviorsSelect.add(new Option(noSaviorsText, noSaviorsValue));
+        
             const deciderGroup = document.createElement('optgroup');
             const observerGroup = document.createElement('optgroup');
             deciderGroup.label = 'Single Decider';
             observerGroup.label = 'Single Observer';
+    
+            
             
             // Define options for savior groups based on the first character of selected animal.
             switch (selectedStack[0]) {
                 case 'S':
-                    deciderGroup.appendChild(new Option('Fi/Si', 'Fi/Si'));
-                    deciderGroup.appendChild(new Option('Fi/Ni', 'Fi/Ni'));
-                    deciderGroup.appendChild(new Option('Ti/Si', 'Ti/Si'));
-                    deciderGroup.appendChild(new Option('Ti/Ni', 'Ti/Ni'));
-                    observerGroup.appendChild(new Option('Si/Fi', 'Si/Fi'));
-                    observerGroup.appendChild(new Option('Si/Ti', 'Si/Ti'));
-                    observerGroup.appendChild(new Option('Ni/Fi', 'Ni/Fi'));
-                    observerGroup.appendChild(new Option('Ni/Ti', 'Ni/Ti'));
+                    this.appendSaviorsOption(deciderGroup, 'Fi/Si');
+                    this.appendSaviorsOption(deciderGroup, 'Fi/Ni');
+                    this.appendSaviorsOption(deciderGroup, 'Ti/Si');
+                    this.appendSaviorsOption(deciderGroup, 'Ti/Ni');
+                    this.appendSaviorsOption(observerGroup, 'Si/Fi');
+                    this.appendSaviorsOption(observerGroup, 'Si/Ti');
+                    this.appendSaviorsOption(observerGroup, 'Ni/Fi');
+                    this.appendSaviorsOption(observerGroup, 'Ni/Ti');
                     break;
                 case 'C':
-                    deciderGroup.appendChild(new Option('Fi/Se', 'Fi/Se'));
-                    deciderGroup.appendChild(new Option('Fi/Ne', 'Fi/Ne'));
-                    deciderGroup.appendChild(new Option('Ti/Se', 'Ti/Se'));
-                    deciderGroup.appendChild(new Option('Ti/Ne', 'Ti/Ne'));
-                    observerGroup.appendChild(new Option('Se/Fi', 'Se/Fi'));
-                    observerGroup.appendChild(new Option('Se/Ti', 'Se/Ti'));
-                    observerGroup.appendChild(new Option('Ne/Fi', 'Ne/Fi'));
-                    observerGroup.appendChild(new Option('Ne/Ti', 'Ne/Ti'));
+                    this.appendSaviorsOption(deciderGroup, 'Fi/Se');
+                    this.appendSaviorsOption(deciderGroup, 'Fi/Ne');
+                    this.appendSaviorsOption(deciderGroup, 'Ti/Se');
+                    this.appendSaviorsOption(deciderGroup, 'Ti/Ne');
+                    this.appendSaviorsOption(observerGroup, 'Se/Fi');
+                    this.appendSaviorsOption(observerGroup, 'Se/Ti');
+                    this.appendSaviorsOption(observerGroup, 'Ne/Fi');
+                    this.appendSaviorsOption(observerGroup, 'Ne/Ti');
                     break;
                 case 'B':
-                    deciderGroup.appendChild(new Option('Fe/Si', 'Fe/Si'));
-                    deciderGroup.appendChild(new Option('Fe/Ni', 'Fe/Ni'));
-                    deciderGroup.appendChild(new Option('Te/Si', 'Te/Si'));
-                    deciderGroup.appendChild(new Option('Te/Ni', 'Te/Ni'));
-                    observerGroup.appendChild(new Option('Si/Fe', 'Si/Fe'));
-                    observerGroup.appendChild(new Option('Si/Te', 'Si/Te'));
-                    observerGroup.appendChild(new Option('Ni/Fe', 'Ni/Fe'));
-                    observerGroup.appendChild(new Option('Ni/Te', 'Ni/Te'));
+                    this.appendSaviorsOption(deciderGroup, 'Fe/Si');
+                    this.appendSaviorsOption(deciderGroup, 'Fe/Ni');
+                    this.appendSaviorsOption(deciderGroup, 'Te/Si');
+                    this.appendSaviorsOption(deciderGroup, 'Te/Ni');
+                    this.appendSaviorsOption(observerGroup, 'Si/Fe');
+                    this.appendSaviorsOption(observerGroup, 'Si/Te');
+                    this.appendSaviorsOption(observerGroup, 'Ni/Fe');
+                    this.appendSaviorsOption(observerGroup, 'Ni/Te');
                     break;
                 case 'P':
-                    deciderGroup.appendChild(new Option('Fe/Se', 'Fe/Se'));
-                    deciderGroup.appendChild(new Option('Fe/Ne', 'Fe/Ne'));
-                    deciderGroup.appendChild(new Option('Te/Se', 'Te/Se'));
-                    deciderGroup.appendChild(new Option('Te/Ne', 'Te/Ne'));
-                    observerGroup.appendChild(new Option('Se/Fe', 'Se/Fe'));
-                    observerGroup.appendChild(new Option('Se/Te', 'Se/Te'));
-                    observerGroup.appendChild(new Option('Ne/Fe', 'Ne/Fe'));
-                    observerGroup.appendChild(new Option('Ne/Te', 'Ne/Te'));
+                    this.appendSaviorsOption(deciderGroup, 'Fe/Se');
+                    this.appendSaviorsOption(deciderGroup, 'Fe/Ne');
+                    this.appendSaviorsOption(deciderGroup, 'Te/Se');
+                    this.appendSaviorsOption(deciderGroup, 'Te/Ne');
+                    this.appendSaviorsOption(observerGroup, 'Se/Fe');
+                    this.appendSaviorsOption(observerGroup, 'Se/Te');
+                    this.appendSaviorsOption(observerGroup, 'Ne/Fe');
+                    this.appendSaviorsOption(observerGroup, 'Ne/Te');
                     break;
                 default:
                     // Keep empty.
                     break;
             }
-    
+        
             // Add option groups to saviors <select>.
             saviorsSelect.add(deciderGroup);
             saviorsSelect.add(observerGroup);
-            
+        
             // Enable saviors selection only when an animal stack is selected.
             saviorsSelect.disabled = !isAnimalStackSelected;
+            saviorsSelect.dispatchEvent(new Event('change'));
         });
-        
-        super(sessionStorage, OP_TYPE_INPUTS_STORAGE_KEY, animalStackSelect, saviorsSelect, modalitySelect);
     
-        /** @type {HTMLSelectElement} */
-        this.animalStackSelect = animalStackSelect;
-        /** @type {HTMLSelectElement} */
-        this.saviorsSelect = saviorsSelect;
-        /** @type {HTMLSelectElement} */
-        this.modalitySelect = modalitySelect;
+        
+        // Triggering animal change because of it being restored in the constructor.
+        animalStackSelect.dispatchEvent(new Event('change'));
     }
     
     /**
@@ -194,7 +233,7 @@ class OpTypeInputs extends StoredInputs {
      * @returns {string|null}
      */
     get #animalStack() {
-        const value = this.animalStackSelect.value;
+        const value = this.#animalStackSelect.value;
         
         return /[SCBP]/.test(value[0]) ? value : null;
     }
@@ -204,7 +243,7 @@ class OpTypeInputs extends StoredInputs {
      * @returns {string|null}
      */
     get #saviors() {
-        const value = this.saviorsSelect.value;
+        const value = this.#saviorsSelect.value;
         return value.includes('/') ? value : null;
     }
     
@@ -213,7 +252,7 @@ class OpTypeInputs extends StoredInputs {
      * @returns {string|null}
      */
     get #modality() {
-        const value = this.modalitySelect.value;
+        const value = this.#modalitySelect.value;
         return /[FM]{2}/.test(value[0]) ? value : null;
     }
     
@@ -232,7 +271,6 @@ class OpTypeInputs extends StoredInputs {
         return this.#is128Valid ? OpType.build512Type(this.#animalStack, this.#saviors, this.#modality) : undefined;
     }
 }
-
 
 
 
@@ -364,84 +402,7 @@ class DiagramStage extends Konva.Stage {
 }
 
 
-// ===== OLD MAIN START (will slowly deleted when each part is replaced/moved) =========================================
 
-
-
-function updateDiagram() {
-    let areAllSelected = true;
-    
-    for (const e of allSelectionElements) {
-        if (e.value.toUpperCase() === 'NONE') {
-            areAllSelected = false;
-            break;
-        }
-    }
-    
-    
-    // Redrawing diagram if a full OP type can be built, or resets to hint to the user to complete the selection.
-    if (areAllSelected) {
-        
-        const isSingleObserver = observerDecider === 'ODD';
-        
-        const opType = new OP.OpType(quadra, isSingleObserver, animals, modality, socialType);
-        // TODO Add double activation numeric hints and masculinity.
-        
-        diagramStage.#drawWithInputs()
-    } else {
-        diagramStage.#resetToHint();
-    }
-}
-
-
-function storeChangedUserInput(isStoringAppearance) {
-    // Storing changed elements as a JSON containing all the elements of the same category. The key is the class name.
-    const dataToStore = {};
-    if (isStoringAppearance) {
-        for (const e of allAppearanceElements) {
-        }
-        
-        
-        localStorage
-    } else {
-        for (const e of allSelectionElements) {
-            let value;
-            if (e instanceof HTMLInputElement) value = e.checked
-            else value = e.value;
-            dataToStore[e.id] = value;
-            console.log(
-                `Storing value ${value} for element ${e.id}`,
-                `Stored value: ${dataToStore[e.id]}`
-            );
-        }
-        
-        sessionStorage.setItem(
-            OP_TYPE_INPUTS_STORAGE_KEY,
-            JSON.stringify(dataToStore)
-        )
-    }
-}
-
-/**
- *
- * @param {Event | string | null} event - Is either an event, null, or a 'skipStoring' string.
- */
-function selectionChangeHandler(event) {
-    const isChangeInAppearance = event?.target?.className === 'appearance-setting';
-    
-    updateDiagram();
-    
-    // noinspection EqualityComparisonWithCoercionJS
-    if (event != 'skipStoring') storeChangedUserInput(isChangeInAppearance);
-}
-
-for (const e of allAppearanceElements) e.addEventListener('change', selectionChangeHandler);
-for (const e of allSelectionElements) e.addEventListener('change', selectionChangeHandler);
-addAllImgLoadEventListener(() => {
-    selectionChangeHandler('skipStoring');
-});
-
-selectionChangeHandler(null);
 // TODO Add listener for page resize.
 // TODO Use "hide" option.
 
@@ -451,6 +412,11 @@ selectionChangeHandler(null);
 // sessionStorage.clear();
 // localStorage.clear();
 
-const diagramStage = new DiagramStage();
-diagramStage.startListening()
+const settings = new DiagramSettings();
+const inputs = new OpTypeInputs();
+
+// HERE Settings seem to be 100% working and ready. Now's time to fix the diagram...
+
+// const diagramStage = new DiagramStage();
+// diagramStage.startListening()
 
