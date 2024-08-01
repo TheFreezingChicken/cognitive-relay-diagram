b08237d2697468331f529824d3b7a539163442ef
The Freezing Chicken <thefreezingchicken+github@gmail.com>
2024-08-01 21:36:11 +0200

    sync (TUTTO TUTTO FUNZIONANTE)

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,10 +3,24 @@ import {CRDStage} from "./relay-diagram.js";
 // =============== MAIN ====================================================================================
 // sessionStorage.clear();
 // localStorage.clear();
+function hideMessage() {
+    console.log('hideMessage called');
+    document.getElementById('help-text').style.display = 'none';
+    localStorage.setItem('helpTextHidden', 'true');
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    const helpTextParagraph = document.getElementById('help-text');
+    helpTextParagraph.addEventListener('click', hideMessage);
+    
+    if (localStorage.getItem('helpTextHidden')) {
+        document.getElementById('help-text').style.display = 'none';
+    }
+    
     CRDStage.initializeResources().then(() => {
         console.log("Resources were marked ready, drawing diagram...");
         new CRDStage(document.getElementById('cognitive-diagram-container'));
     });
 });
+
